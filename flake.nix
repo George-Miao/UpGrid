d0f81bc6f93267e6efb1c5583476cd23a93fc39c
George Miao <gm@miao.dev>
2026-08-24 23:31:50 +0900

    ci: strengthen package and container checks

--- flake.nix
+++ flake.nix
@@ -14,7 +14,11 @@
     flake-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ] (
       system: let
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
@@ -39,6 +43,40 @@
           doCheck = false;
           meta.mainProgram = "upgrid";
         };
+        containerHealthcheck = pkgs.writeShellApplication {
+          name = "upgrid-healthcheck";
+          runtimeInputs = [pkgs.curl];
+          text = ''
+            bind="''${UPGRID_BIND:-0.0.0.0:8080}"
+            port="''${bind##*:}"
+            case "$bind" in
+              "[::]:"*)
+                host="[::1]"
+                ;;
+              "["*"]:"*)
+                host="''${bind%:*}"
+                ;;
+              *)
+                host="''${bind%:*}"
+                if [[ "$host" == "0.0.0.0" ]]; then
+                  host="127.0.0.1"
+                fi
+                ;;
+            esac
+            scheme="http"
+            curl_options=(--fail --silent --show-error)
+            if [[ -n "''${UPGRID_TLS_CERT:-}" ]]; then
+              scheme="https"
+              curl_options+=(--cacert "$UPGRID_TLS_CERT")
+            fi
+            if [[ -n "''${UPGRID_HEALTH_URL:-}" ]]; then
+              url="$UPGRID_HEALTH_URL"
+            else
+              url="''${scheme}://''${host}:''${port}/healthz"
+            fi
+            exec curl "''${curl_options[@]}" "$url"
+          '';
+        };
         runtimeRoot = pkgs.runCommand "upgrid-runtime-root" {} ''
           install -D -m 0755 ${upgrid}/bin/upgrid $out/usr/local/bin/upgrid
         '';
@@ -47,7 +85,9 @@
           tag = "nix";
           contents = [
             runtimeRoot
+            containerHealthcheck
             pkgs.dockerTools.caCertificates
+            pkgs.curl
           ];
           extraCommands = ''
             mkdir -p etc var/lib/upgrid
@@ -63,6 +103,13 @@
               "UPGRID_BIND=0.0.0.0:8080"
               "UPGRID_DATA_DIR=/var/lib/upgrid"
             ];
+            Healthcheck = {
+              Test = ["CMD" "/bin/upgrid-healthcheck"];
+              Interval = 30 * 1000 * 1000 * 1000;
+              Timeout = 5 * 1000 * 1000 * 1000;
+              Retries = 3;
+              StartPeriod = 5 * 1000 * 1000 * 1000;
+            };
             ExposedPorts = {
               "8080/tcp" = {};
               "11451/udp" = {};
